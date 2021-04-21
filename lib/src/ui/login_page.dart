027aecefa88f039f25b6d467202466cc1f4115ab
EmmanuelMontes111 <emmanuelmontes111@gmail.com>
2021-04-20 21:13:59 -0500

    Login page background design

--- lib/src/ui/login_page.dart
+++ lib/src/ui/login_page.dart
@@ -1,11 +1,73 @@
 import 'package:flutter/material.dart';
+
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Login Page'),
+        body: Stack(
+      children: <Widget>[
+        _createBackground(context),
+      ],
+    ));
+  }
+
+  Widget _createBackground(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+    final blueBackground = Container(
+      height: screenSize.height * 0.4,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: <Color>[
+          Color.fromRGBO(4, 75, 172, 1.0),
+          Color.fromRGBO(31, 82, 194, 1.0)
+        ]),
       ),
+    );
+
+    final circle = Container(
+      width: 100.0,
+      height: 100.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(100.0),
+        color: Color.fromRGBO(255, 255, 255, 0.05),
+      ),
+    );
+
+    final logoImage = Container(
+      child: Container(
+        height: 200.0,
+        width: 200.0,
+        decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            image: DecorationImage(
+                image: AssetImage('assets/img/medium_logo.png'),
+               ),
+        ),
+        ),
+      );
+    return Stack(
+      children: <Widget>[
+        blueBackground,
+        Positioned(top: 90.0, left: 30.0, child: circle),
+        Positioned(top: -40.0, right: -30.0, child: circle),
+        Positioned(top: 90.0, left: 30.0, child: circle),
+        Positioned(bottom: -50.0, right: -10.0, child: circle),
+        Positioned(bottom: 120.0, right: 20.0, child: circle),
+        Positioned(bottom: -50.0, left: -20.0, child: circle),
+        Container(
+          padding: EdgeInsets.only(top: 10.0),
+          child: Column(
+            children: <Widget>[
+              logoImage,
+              SizedBox(height: 10.0, width: double.infinity,),
+              Text(
+                'BrigadaApp',
+                style: TextStyle(color: Colors.white, fontSize: 25.0),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
