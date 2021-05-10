b5f095de96953709190de5a98a584859c567aae6
EmmanuelMontes111 <emmanuelmontes111@gmail.com>
2021-05-09 22:40:20 -0500

    ImplementsPage is replaced by implements2Page

--- lib/src/ui/implements_page.dart
+++ lib/src/ui/implements_page.dart
@@ -1,122 +1,136 @@
+import 'package:brigadapoli/src/ui/list_head_phones_page.dart';
+import 'package:brigadapoli/src/ui/list_kits_page.dart';
+import 'package:brigadapoli/src/ui/list_radios_page.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import 'list_head_phones_page.dart';
-import 'list_kits_page.dart';
-import 'list_radios_page.dart';
-
 // ignore: must_be_immutable
-class ImplementsPage extends StatefulWidget {
+class Implements2Page extends StatefulWidget {
   @override
-  _ImplementsPageState createState() => _ImplementsPageState();
+  _Implements2PageState createState() => _Implements2PageState();
 }
 
-class _ImplementsPageState extends State<ImplementsPage> {
-  Map<String, Widget> tabsIcons = {
-    'Radio': CircleAvatar(
-      backgroundColor: Colors.white,
-      child: Icon(Icons.radio),
+class _Implements2PageState extends State<Implements2Page> {
+  int theriGroupValue = 0;
+
+  final Map<int, Widget> pageWidgets = const <int, Widget>{
+    0: Text(
+      "Radios",
+      style: TextStyle(
+        color: Colors.black12,
+        decoration: TextDecoration.none,
+        fontSize: 20.0,
+      ),
     ),
-    'Botiquin': CircleAvatar(
-      backgroundColor: Colors.white,
-      child: Icon(Icons.work),
+    1: Text(
+      "Botiquínes",
+      style: TextStyle(
+        color: Colors.black12,
+        decoration: TextDecoration.none,
+        fontSize: 20.0,
+      ),
     ),
-    'Audifonos': CircleAvatar(
-      backgroundColor: Colors.white,
-      child: Icon(Icons.headset_mic_rounded),
+    2: Text(
+      "Audifonos",
+      style: TextStyle(
+        color: Colors.black12,
+        decoration: TextDecoration.none,
+        fontSize: 20.0,
+      ),
     ),
   };
 
-  Widget _floatingActionButton;
+  List<Widget> bodies = [
+    ListRadiosPage(),
+    ListKitsPage(),
+    ListHeadPhonesPage(),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    List<Tab> androidTabs = [];
-    tabsIcons.forEach((key, value) {
-      androidTabs.add(
-        Tab(
-          icon: value,
-          text: key,
-        ),
-      );
-    });
-
-    _floatingActionButton = FloatingActionButton(
-      child: Icon(
-        Icons.add,
-        color: Color.fromRGBO(4, 75, 172, 1.0),
-      ),
-      elevation: 10.0,
-      backgroundColor: Colors.white,
-      onPressed: () => Navigator.pushNamed(context, 'addkits'),
-    );
-
-    return DefaultTabController(
-      length: tabsIcons.length,
-      child: Scaffold(
-        floatingActionButton: _floatingActionButton,
-        appBar: AppBar(
-          bottom: TabBar(
-            indicatorColor: Colors.white,
-            labelStyle: TextStyle(fontSize: 16.0),
-            unselectedLabelStyle: TextStyle(fontSize: 14),
-            tabs: androidTabs,
-            isScrollable: true,
+    return Scaffold(
+      floatingActionButton: _floatingActionButtonTapBodies(),
+      body: bodies[theriGroupValue],
+      appBar: AppBar(
+        bottom: PreferredSize(
+          preferredSize: Size(double.infinity, 45.0),
+          child: Padding(
+            padding: EdgeInsets.only(top: 5.0,bottom: 10.0),
+            child: Row(
+              children: <Widget>[
+                SizedBox(width: 15.0,),
+                Expanded(
+                  child: CupertinoSegmentedControl(
+                    groupValue: theriGroupValue,
+                    onValueChanged: (value) {
+                      setState(() {
+                        theriGroupValue = value;
+                      });
+                    },
+                    children: pageWidgets,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
-        body: TabBarView(
-          children: <Widget>[
-            _listRadioPage(context),
-            _listKitsPage(context),
-            _listHeadPhonesPag(context),
-          ],
-        ),
       ),
     );
   }
 
-  Widget _listRadioPage(BuildContext context) {
-    setState(() {
-      _floatingActionButton = FloatingActionButton(
-        child: Icon(
-          Icons.add,
-          color: Color.fromRGBO(4, 75, 172, 1.0),
-        ),
-        elevation: 10.0,
-        backgroundColor: Colors.white,
-        onPressed: () => Navigator.pushNamed(context, 'addHeadPhones'),
-      );
-    });
-    return ListRadiosPage();
+  // ignore: missing_return
+  Widget _floatingActionButtonTapBodies(){
+    switch(theriGroupValue) {
+      case 0: {
+        return FloatingActionButton(
+          child: Icon(
+            Icons.add,
+            color: Color.fromRGBO(4, 75, 172, 1.0),
+          ),
+          elevation: 10.0,
+          backgroundColor: Colors.white,
+          onPressed: () => Navigator.pushNamed(context, 'addRadios'),
+        );
+      }
+      break;
+
+      case 1: {
+        return FloatingActionButton(
+          child: Icon(
+            Icons.add,
+            color: Color.fromRGBO(4, 75, 172, 1.0),
+          ),
+          elevation: 10.0,
+          backgroundColor: Colors.white,
+          onPressed: () => Navigator.pushNamed(context, 'addkits'),
+        );
+      }
+      break;
+
+      case 2: {
+        return FloatingActionButton(
+          child: Icon(
+            Icons.add,
+            color: Color.fromRGBO(4, 75, 172, 1.0),
+          ),
+          elevation: 10.0,
+          backgroundColor: Colors.white,
+          onPressed: () => Navigator.pushNamed(context, 'addHeadPhones'),
+        );
+      }
+      break;
+      default:
+        return FloatingActionButton(
+          child: Icon(
+            Icons.add,
+            color: Color.fromRGBO(4, 75, 172, 1.0),
+          ),
+          elevation: 10.0,
+          backgroundColor: Colors.white,
+          onPressed: () => Navigator.pushNamed(context, 'addHeadPhones'),
+        );
+        break;
+    }
   }
 
-  Widget _listKitsPage(BuildContext context) {
-    setState(() {
-      _floatingActionButton = FloatingActionButton(
-        child: Icon(
-          Icons.add,
-          color: Color.fromRGBO(4, 75, 172, 1.0),
-        ),
-        elevation: 10.0,
-        backgroundColor: Colors.white,
-        onPressed: () => Navigator.pushNamed(context, 'addkits'),
-      );
-    });
-
-    return ListKitsPage();
-  }
-
-  Widget _listHeadPhonesPag(BuildContext context) {
-    setState(() {
-      _floatingActionButton = FloatingActionButton(
-        child: Icon(
-          Icons.add,
-          color: Color.fromRGBO(4, 75, 172, 1.0),
-        ),
-        elevation: 10.0,
-        backgroundColor: Colors.white,
-        onPressed: () => Navigator.pushNamed(context, 'addHeadPhones'),
-      );
-    });
-    return ListHeadPhonesPage();
-  }
 }
