c5e6791ae79eb74ba86dd52d3c553c38d9f1d00d
EmmanuelMontes111 <emmanuelmontes111@gmail.com>
2021-05-18 23:00:55 -0500

    validation login is implemented

--- lib/src/providers/user_provider.dart
+++ lib/src/providers/user_provider.dart
@@ -5,25 +5,28 @@ import 'package:http/http.dart' as http;
 class UserProvider {
   final String _firebaseToken = 'AIzaSyDa1eJoc5djVqRdWQrCs89DTi7g8-jZJ1E';
 
-  Future<Map<String, dynamic>> login(String email, String password) async {
-    final authData = {
-      'email': email,
-      'password': password,
-      'returnSecureToken': true,
-    };
-    final response = await http.post(
-        Uri.parse(
-            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=$_firebaseToken'),
-        body: json.encode(authData));
 
-    Map<String, dynamic> decodedResponse = json.decode(response.body);
+  Future<Map<String, dynamic>> login(String email, String password) async {
+
+    final authData = {
+      'email':  email,
+      'password':  password,
+      'returnSecureToken':  true,
+    };
+
+    final response =  await http.post(
+        Uri.parse(
+             'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=$_firebaseToken'),
+        body:  json.encode(authData));
+
+    Map<String, dynamic>  decodedResponse  =  json.decode(response.body);
 
     print(decodedResponse);
     print(response);
-    if (decodedResponse.containsKey('idToken')) {
-      return {'ok': true};
+    if ( decodedResponse.containsKey('idToken') ) {
+       return {'ok': true};
     } else {
-      return {'ok': true, 'mensaje': decodedResponse['message']};
+       return {'ok': true, 'mensaje': decodedResponse['message']};
     }
   }
 
