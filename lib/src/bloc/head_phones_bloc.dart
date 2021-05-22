b75cbecbcc24ff02b74f0417fb5b2eee19034e34
EmmanuelMontes111 <emmanuelmontes111@gmail.com>
2021-05-21 21:56:58 -0500

    loadHeadPhones method is created in HeadPhonesBloc class

--- lib/src/bloc/head_phones_bloc.dart
+++ lib/src/bloc/head_phones_bloc.dart
@@ -2,16 +2,23 @@ import 'package:brigadapoli/src/models/head_phone_model.dart';
 import 'package:brigadapoli/src/providers/head_phones_provider.dart';
 import 'package:rxdart/rxdart.dart';
 
-class HeadPhonesBloc{
+class HeadPhonesBloc {
   final _headPhoneController = new BehaviorSubject<List<HeadPhoneModel>>();
   final _loadController = new BehaviorSubject<bool>();
 
   final _headPhonesProvider = new HeadPhonesProvider();
 
-  Stream<List<HeadPhoneModel>>  get headPhonesStream => _headPhoneController.stream;
-  Stream<bool>  get loadStream => _loadController.stream;
+  Stream<List<HeadPhoneModel>> get headPhonesStream =>
+      _headPhoneController.stream;
 
-  dispose(){
+  Stream<bool> get loadStream => _loadController.stream;
+
+  void loadHeadPhones() async {
+    final headPhones = await _headPhonesProvider.loadHeadPhones();
+    _headPhoneController.sink.add(headPhones);
+  }
+
+  dispose() {
     _headPhoneController?.close();
     _loadController?.close();
   }
