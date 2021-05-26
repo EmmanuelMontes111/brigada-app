fa540cb139848a93307d31c21c42d79e20308a70
EmmanuelMontes111 <emmanuelmontes111@gmail.com>
2021-05-26 16:11:54 -0500

    HeadPhoneBloc is added to provider inheritedWidget

--- lib/src/bloc/provider.dart
+++ lib/src/bloc/provider.dart
@@ -1,9 +1,14 @@
-
 import 'package:flutter/material.dart';
 
 import 'login_bloc.dart';
 
+import 'package:brigadapoli/src/bloc/head_phones_bloc.dart';
+export 'package:brigadapoli/src/bloc/head_phones_bloc.dart';
+
 class Provider extends InheritedWidget {
+
+  final loginBloc = new LoginBloc();
+  final _headPhoneBloc = new HeadPhonesBloc();
 
   static Provider _instanceProvider;
   factory Provider({Key key, Widget child}){
@@ -16,12 +21,16 @@ class Provider extends InheritedWidget {
   Provider._internal({Key key, Widget child})
       : super(key: key,child: child);
 
-  final loginBloc = LoginBloc();
+
 
   @override
   bool updateShouldNotify(covariant InheritedWidget oldWidget) => true;
 
   static LoginBloc of(BuildContext context) {
     return context.dependOnInheritedWidgetOfExactType<Provider>().loginBloc;
+  }
+
+  static HeadPhonesBloc headPhoneBloc (BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<Provider>()._headPhoneBloc;
   }
 }
