3613759202d242c7d3e5da52ae439c4cf3a6ecdc
EmmanuelMontes111 <emmanuelmontes111@gmail.com>
2021-06-04 18:23:13 -0500

    Buttons are added to choose implements

--- lib/src/ui/profile_page.dart
+++ lib/src/ui/profile_page.dart
@@ -1,12 +1,55 @@
 import 'package:flutter/material.dart';
-class ProfilePage extends StatelessWidget{
+class ProfilePage extends StatefulWidget{
 
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Center(
-      child: Text('Perfil',style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22)),
+      child: Column(
+        children: <Widget>[
+
+          Column(mainAxisSize: MainAxisSize.max, children: <Widget>[
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: ElevatedButton(
+                      style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.white60)),
+                      child: Text("Elija Un Botiquín",style: TextStyle(color: Colors.black26),),
+                      onPressed: () {}),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: ElevatedButton(
+                      style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.white60)),
+                      child: Text("Elija Un Botiquín",style: TextStyle(color: Colors.black26),),
+                      onPressed: () {}),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: ElevatedButton(
+                      style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.white60)),
+                      child: Text("Elija Un Botiquín",style: TextStyle(color: Colors.black26),),
+                      onPressed: () {}),
+                ),
+              ],
+            ),
+          ]),
+        ],
+      ),
     );
   }
-
 }
