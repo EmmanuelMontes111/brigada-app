e68feaa516a800477bf6781620ea451f5f020040
EmmanuelMontes111 <emmanuelmontes111@gmail.com>
2021-06-10 10:14:25 -0500

    emailController and passwordController are added to RegisterBloc

--- lib/src/bloc/register_bloc.dart
+++ lib/src/bloc/register_bloc.dart
@@ -6,18 +6,27 @@ import 'package:rxdart/rxdart.dart';
 
 class RegisterBloc with Validators{
   final _nameCrontroller = BehaviorSubject<String>();
+  final _emailCrontroller = BehaviorSubject<String>();
+  final _passwordCrontroller = BehaviorSubject<String>();
 
   // Recuperar los datos del Stream
   Stream<String> get nameStream => _nameCrontroller.stream.transform(validateName);
+  Stream<String> get emailStream => _emailCrontroller.stream.transform(validateEmail);
+  Stream<String> get passwordStream => _passwordCrontroller.stream.transform(validatePassword);
 
   // Insertar valores al Stream
   Function(String) get changedName => _nameCrontroller.sink.add;
-
+  Function(String) get changedEmail => _emailCrontroller.sink.add;
+  Function(String) get changedPassword => _passwordCrontroller.sink.add;
 
   // Obtener el último valor ingresado a los streams
   String get name => _nameCrontroller.value;
+  String get email => _emailCrontroller.value;
+  String get password => _passwordCrontroller.value;
 
   dispose(){
     _nameCrontroller?.close();
+    _emailCrontroller?.close();
+    _passwordCrontroller?.close();
   }
 }
--- test/bloc/register_bloc_test.dart
+++ test/bloc/register_bloc_test.dart
@@ -24,4 +24,35 @@ void main() {
         .onError((handleError) => expect(handleError, "Tu nombre es requerido"));
     _bloc.changedName(nameExpected);
   });
+
+  test('validate Email when is valid', () {
+    String emailExpected = "emmanuelmontes@gmail.com";
+    _bloc.emailStream.listen((email) {
+      expect(email, emailExpected);
+    });
+    _bloc.changedEmail(emailExpected);
+  });
+
+  test('validate Email when not is valid', () {
+    String emailExpected = "emmanuelmontes";
+    _bloc.emailStream
+        .listen((email) => {})
+        .onError((handleError) => expect(handleError, "Email no valido"));
+    _bloc.changedEmail(emailExpected);
+  });
+
+  test('validate Password when it has more than 6 characters', () {
+    String passwordExpected = "123456789";
+    _bloc.passwordStream.listen((password) {
+      expect(password, passwordExpected);
+    });
+    _bloc.changedPassword(passwordExpected);
+  });
+
+  test('validate Password when it has less than 6 characters', () {
+    String passwordExpected = "123";
+    _bloc.passwordStream.listen((password) => {}).onError(
+            (handleError) => expect(handleError, "Debe tener más de 6 caracteres"));
+    _bloc.changedPassword(passwordExpected);
+  });
 }
